4ff858d323dc58350856dedc6a17e5a59ba3d88b
Naji Hiba <najihiba137@gmail.com>
2024-05-28 12:26:15 +0100

    add ismail's page

---
+++ src/app/team/IsmailELHANI/page.jsx
@@ -0,0 +1,106 @@
+"use client"
+import Link from 'next/link'
+import React from 'react'
+
+function ismail() {
+    return (
+
+        <div className="flex flex-col md:flex-row">
+            <div className="w-full md:w-1/3 order-first md:order-2 mt-28 md:mt-0">
+                <div className="relative h-full w-full flex items-center justify-center sm:mb-7">
+                    <div className="profileCard_container relative p-10 border-2 border-dashed rounded-full border-spacing-4 border-gray-400 max-w-2xl ">
+                        <button className="profile_item left-[45px] -top-[4px] absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className=' rounded-lg' src="/icons_team/trello-logo.png" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item right-[45px] -top-[4px] absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className='rounded-full' src="/icons_team/googleads-logo.png" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item -left-4 top-20 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className='rounded-full' src="/icons_team/instagram-logo.png" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item -right-4 top-20 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className='rounded-full' src="/icons_team/facebook-logo.png" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item bottom-8 -left-0 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className='rounded-md' src="/icons_team/Canva-logo.png" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item bottom-8 -right-0 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className='rounded-full' src="/icons_team/capcut-logo.png" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item right-[40%] -bottom-4 absolute rounded-full bg-cover cursor-pointer border border-gray-400/50 p-[2px] active:scale-95 hover:scale-95 transition-all duration-500">
+                            <span className="block w-[40px] h-[40px] transition-all duration-500 rounded-full z-[2] bg-white p-1">
+                                <img className='rounded-lg w-10 h-8' src="/icons_team/TikTok-Logo.jpg" alt="" />
+                            </span>
+                        </button>
+
+                        <button className="profile_item w-[200px] h-[200px] p-1 border-2 rounded-full hover:border-gray-400/50 cursor-pointer transition-all duration-500 z-0">
+                            <div className="w-full bg-white h-full flex items-center justify-center p-2 rounded-full active:scale-95 hover:scale-95 object-cover transition-all duration-500">
+                                <span className="w-42 h-42 inline-block"><a href="mailto:ismailelhani2017@gmail.com">
+                                    <img src="/photo-profile/ismail's profile.png" alt="hiba's profile" className='rounded-full' /></a>
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full md:w-2/3 md:order-1">
+                <div className="flex flex-col m-4 md:ml-8 md:mt-8 items-start justify-center min-h-screen">
+                    <div className="w-full backdrop-blur-lg bg-black/60 rounded-3xl shadow-2xl p-4">
+                        <div className="flex p-2 gap-1">
+                            <div className="circle">
+                                <span className="bg-blue-500 inline-block center w-3 h-3 rounded-full"></span>
+                            </div>
+                            <div className="circle">
+                                <span className="bg-purple-500 inline-block center w-3 h-3 rounded-full"></span>
+                            </div>
+                            <div className="circle">
+                                <span className="bg-pink-500 box inline-block center w-3 h-3 rounded-full"></span>
+                            </div>
+                        </div>
+                        <br />
+                        <h2 className='text-xl md:text-3xl font-bold text-white'>Ismail EL HANI from Vision<span className='text-[#CB6CE6]'>360°</span></h2>
+                        <h3 className='text-md md:text-lg font-semibold text-blue-500 italic'>The Social Media Maestro!</h3> <br />
+                        <div className='text-gray-300'>
+                            Hello, dear future clients,<br />
+                            Envision a master of engagement, a strategist who turns social media platforms into stages where brands perform and audiences applaud. Your search ends here, because Ismail EL HANI from Vision360° is the virtuoso ready to amplify your online presence!<br /><br />
+                            <span className='text-pink-500 ml-4 font-semibold'>Who is Ismail EL HANI?</span> <br />
+                            Ismail is more than a social media manager; he's our team's conductor, orchestrating campaigns that resonate and connect. His profound understanding of digital landscapes allows him to navigate and innovate within any social platform, ensuring your brand not only participates but stands out in the digital crowd.<br /><br />
+                            <span className='text-pink-500 ml-4 font-semibold'>Why choose Ismail EL HANI?</span> <br />
+                            With Ismail's expertise, your social media will transform into captivating hubs of interaction. His strategic foresight is unmatched, crafting campaigns that are not only creative but also drive engagement and growth. His attention to detail and knack for trend-spotting make him an invaluable asset in managing your digital footprint effectively, turning followers into loyal fans.<br /><br />
+                            <div className='justify-center items-center text-center'> <div className='underline text-blue-500'>Click Ismail's avatar to ask anything!</div>
+                            </div>
+
+                            <br />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    )
+}
+
+export default ismail;
